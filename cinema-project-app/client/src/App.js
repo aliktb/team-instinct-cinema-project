fe413c0934c960154d1c795d9729f0c6df946cbb
Adam Mussa <adammussa1999@gmail.com>
2021-12-06 15:02:06 +0000

    CIN-1 #Added-styling-to-navbar
    smoothed corners and made styled

--- cinema-project-app/client/src/App.js
+++ cinema-project-app/client/src/App.js
@@ -2,6 +2,7 @@ import "./css/App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import TestAPIPage from "./pages/TestAPIPage";
+import Navigationbar from "./component/Navigationbar";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             element={<p>change me from client/app.js</p>}
           ></Route>
           <Route path="test" element={<TestAPIPage />}></Route>
+          <Route path="navBar" element={<Navigationbar />}></Route>
         </Routes>
       </Router>
     </div>
