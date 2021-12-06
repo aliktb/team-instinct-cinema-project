e173c411061d42c58a8a9eb4eee94f8261cec8c4
Adam Mussa <adammussa1999@gmail.com>
2021-12-06 14:34:39 +0000

    CIN-21 #Background-Colour-Element-created

--- cinema-project-app/client/src/App.js
+++ cinema-project-app/client/src/App.js
@@ -1,7 +1,9 @@
 import "./css/App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+
 import TestAPIPage from "./pages/TestAPIPage";
+import Background from "./Components/Background";
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
             element={<p>change me from client/app.js</p>}
           ></Route>
           <Route path="test" element={<TestAPIPage />}></Route>
+          <Route path="background" element={<Background />}></Route>
         </Routes>
       </Router>
     </div>
