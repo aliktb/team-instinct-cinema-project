1649f985460587413173c43a643bd2c4938319e4
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-12-06 13:32:35 +0000

    CIN-17 #comment created movie.js persistence file for modelling movies

---
+++ cinema-project-app/server/persistence/movie.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const actorSchema = new Schema({
+    name: String
+})
+
+const movieSchema = new Schema({
+    title: String,
+    rating: Number,
+    runtime: Number,
+    cast: actorSchema,
+    imageUrl: String,
+    release: Date
+})
+
+const Movie = model('Movie', movieSchema);
+
+module.exports = { 'Movie': Movie };
