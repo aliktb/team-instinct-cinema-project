1a8a6b21441a0cd2c4d8533f68353281cc3e6e0f
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-12-06 16:26:31 +0000

    CIN-18 #comment added delete booking function

--- cinema-project-app/server/routes/bookings.js
+++ cinema-project-app/server/routes/bookings.js
@@ -42,3 +42,13 @@ router.post('/create', (req, res, next) => {
         res.status(500).send(error);
     })
 })
+
+router.delete('/delete/:id', (req, res, next) => {
+    Booking.findByIdAndDelete(req.params.id, (error, result) => {
+        if (error) {
+            res.status(404).send(error.message);
+        } else {
+            res.status(204)
+        }
+    })
+})
