9fa3b49186f5ea18829f5064435be6ad11084d7c
Adam Mussa <adammussa1999@gmail.com>
2021-12-07 14:24:06 +0000

    Added Places To go page
    more info and pictures needed on this page.

--- cinema-project-app/client/src/App.js
+++ cinema-project-app/client/src/App.js
@@ -1,13 +1,14 @@
 import "./css/App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import TestAPIPage from "./pages/TestAPIPage";
+// import TestAPIPage from "./pages/TestAPIPage";
 
 import GettingHere from "./pages/GettingHere";
 
 import Navigationbar from "./Components/Navigationbar";
 import Homepage from "./pages/Homepage";
-
+import PlacesToGo from "./pages/PlacesToGo";
+// import FilmClass from "./pages/FilmClass";
 
 
 
@@ -25,11 +26,11 @@ function App() {
             {/* <Route path="/Listing_Gallery" element={<ListGallery />}></Route>
             <Route path="/New_Listings" element={<NewListings />}></Route>
             <Route path="/Bookings" element={<Bookings />}></Route>
-            <Route path="/DiscussionBoard" element={<DiscussionBoard />}></Route>
-            <Route path="/Film_Classification" element={<FilmClass />}></Route>
-            <Route path="/About" element={<AboutUs />}></Route> */}
+            <Route path="/DiscussionBoard" element={<DiscussionBoard />}></Route> */}
+            {/* <Route path="/Film_Classification" element={<FilmClass />}></Route> */}
+            {/* <Route path="/About" element={<AboutUs />}></Route> */}
             <Route path="/Getting_Here" element={<GettingHere />}></Route>
-            {/* <Route path="/Places_to_go" element={<Places />}></Route> */}
+            <Route path="/Places_to_go" element={<PlacesToGo />}></Route>
             {/* <Route path="/Contact_us" element={<Contact />}></Route> */}
 
 
---
+++ cinema-project-app/client/src/pages/PlacesToGo.jsx
@@ -0,0 +1,41 @@
+        const PlacesToGo = () => {
+    return (
+        <div>
+
+            <header>
+
+            </header>
+            <h2> Things you can do  </h2>
+            <h4> Take a tour around the the Historic site!</h4>
+            <img src="" alt="buckingham palace image" />
+            <h5> visit the :State Rooms <br />
+            Whenever Her Majesty is away for her summer holidays, the State Rooms inside Buckingham Palace are opened to the public, giving you a chance to look behind-the-scenes at one of the world’s most famous royal residences. <br />
+            Royal Mews <br />The Mews is home to the Royal Family’s carriages and is responsible for all their travel arrangements throughout the year. A fascinating behind-the-scenes glimpse into all the work that surrounds the Royal Family, the Mews is open from February to November each year.
+            <br /> Queen’s Gallery <br />
+            An art gallery attached to Buckingham Palace, the Queen’s Gallery features a rotating collection of works from the Royal Collection and is open all year ‘round.
+
+            <br />Changing of the Guard <br />
+            A trip to London isn’t complete without watching the Changing of the Guard. This world-famous ceremony takes place daily in the summer and around 4 times a week in the off-season.
+            It’s totally free to do (it's #1 of 10 top 10 free things to do in London) and we’ve written a helpful guide to show you the best places to watch.
+            Or, let us do the work, by joining us on our Westminster Tour where we incorporate the Changing of the Guard into the tour whenever it is taking place!</h5>
+            <h2> Have a Drink At The Local Pub </h2>
+            <h5> visit the local pub and have yourself some royally good fun</h5>
+            <h2> VISIT SOME OF THE LOCAL TOURIST SITES </h2>
+            <h5> Take a tour around the the Historic site! <br /> we have a museum of artifacts stole from across the globe. </h5>
+
+
+            <img src="" alt="image if buckingham Palace grounds" />
+
+            <footer>
+
+
+
+            </footer>
+
+
+        </div>
+
+    );
+}
+
+export default PlacesToGo;
