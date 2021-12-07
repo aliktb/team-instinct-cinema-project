50445fe28caf1f74aea46a57e594a5d6a1e24aba
Eamon Moradi <eamonmoradi@Eamons-MacBook-Air.local>
2021-12-07 17:11:08 +0000

    #CIN-12 #comment adding about us page

---
+++ cinema-project-app/client/src/pages/AboutUs.jsx
@@ -0,0 +1,20 @@
+const AboutUs = () => {
+    return (
+        <div>
+            <h2>A Little Bit About QA Cinemas!...</h2>
+            <h3>QA Cinemas have been providing you with action packed entertainment for over 20 years!</h3>
+
+
+
+        </div>
+
+    );
+}
+
+export default AboutUs;
+<div>
+
+
+
+
+</div>
