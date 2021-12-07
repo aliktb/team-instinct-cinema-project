d61872549270886aba9dd27eedd1b6c7ed827e9f
Eamon Moradi <eamonmoradi@Eamons-MacBook-Air.local>
2021-12-07 12:44:25 +0000

    #cin-1 #comment adding film classifications page

--- cinema-project-app/client/src/App.js
+++ cinema-project-app/client/src/App.js
@@ -7,7 +7,7 @@ import GettingHere from "./pages/GettingHere";
 
 import Navigationbar from "./Components/Navigationbar";
 import Homepage from "./pages/Homepage";
-
+import FilmClass from "./pages/FilmClass";
 
 
 
@@ -25,9 +25,9 @@ function App() {
             {/* <Route path="/Listing_Gallery" element={<ListGallery />}></Route>
             <Route path="/New_Listings" element={<NewListings />}></Route>
             <Route path="/Bookings" element={<Bookings />}></Route>
-            <Route path="/DiscussionBoard" element={<DiscussionBoard />}></Route>
+            <Route path="/DiscussionBoard" element={<DiscussionBoard />}></Route> */}
             <Route path="/Film_Classification" element={<FilmClass />}></Route>
-            <Route path="/About" element={<AboutUs />}></Route> */}
+            {/* <Route path="/About" element={<AboutUs />}></Route> */}
             <Route path="/Getting_Here" element={<GettingHere />}></Route>
             {/* <Route path="/Places_to_go" element={<Places />}></Route> */}
             {/* <Route path="/Contact_us" element={<Contact />}></Route> */}
