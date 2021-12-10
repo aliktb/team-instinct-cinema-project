6be6a65dcc11fd2f32074187040298662991fe43
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-12-10 14:18:18 +0000

    CIN-13 #comment added payment components

---
+++ cinema-project-app/client/src/Components/PaymentProvider.jsx
@@ -0,0 +1,74 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { useEffect, useState } from 'react';
+import Payment from './Payment';
+
+const PaymentProvider = ({ booking }) => {
+
+    const [clientSecret, setClientSecret] = useState("");
+
+    const stripePromise = loadStripe(
+
+        'pk_test_51K4meeA1FgqwOtqeXbVgZ4cClXc8WWXcLXHsvZClvjM0BEGXlESLfQUrwzhGtdhFzFUxx05CHMWADPBnAfY8bMDj00KaitBrJx'
+    );
+
+
+
+
+    useEffect(() => {
+
+        // Create PaymentIntent as soon as the page loads
+
+        fetch("http://localhost:3001/payments/create-payment-intent", {
+
+            method: "POST",
+
+            headers: { "Content-Type": "application/json" },
+
+            body: JSON.stringify({ booking }),
+
+        })
+
+            .then((res) => res.json())
+
+            .then((data) => setClientSecret(data.clientSecret));
+
+    }, []);
+
+
+    const appearance = {
+
+        theme: 'stripe',
+
+    };
+
+    const options = {
+
+        clientSecret,
+
+        appearance,
+
+    };
+
+    return (
+
+        <div className="App">
+
+            {clientSecret && (
+
+                <Elements options={options} stripe={stripePromise}>
+
+                    <Payment booking={booking} />
+
+                </Elements>
+
+            )}
+
+        </div>
+
+    );
+
+
+}
+
+export default PaymentProvider;
