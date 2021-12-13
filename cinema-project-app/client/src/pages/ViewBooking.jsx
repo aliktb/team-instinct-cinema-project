942c52908b7252d9fc45026ff459a19d6afd0c89
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-12-13 12:06:07 +0000

    CIN-38 #comment created booking viewing page

---
+++ cinema-project-app/client/src/pages/ViewBooking.jsx
@@ -0,0 +1,75 @@
+import axios from "axios";
+import { useState } from "react";
+import { Button, Input } from 'reactstrap'
+
+
+const ViewBooking = () => {
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [bookingDetails, setBookingDetails] = useState(null);
+    const [code, setCode] = useState("");
+    const [bookingDetailsFound, setBookingDetailsFound] = useState(false);
+
+    const searchType = (searchTerm) => {
+        if (searchTerm.length === 8) {
+            axios.get(`http://localhost:3001/bookings/ref/${searchTerm}`).then((data) => {
+                setBookingDetails(data.data);
+                if (data.data > 0) {
+                    setBookingDetailsFound(true);
+                }
+                setIsLoaded(true);
+            })
+        } else {
+            axios.get(`http://localhost:3001/bookings/${searchTerm}`).then((data) => {
+                setBookingDetails(data.data);
+                if (data.data > 0) {
+                    setBookingDetailsFound(true);
+                }
+                setIsLoaded(true);
+            })
+        }
+    }
+
+
+
+
+    if (isLoaded) {
+
+        // if (bookingDetailsFound) {
+
+
+
+        return (
+            <div>
+                <p>Booking Reference: {bookingDetails._id}</p>
+                <p>Booking code: {bookingDetails.bookingRef}</p>
+                <p>Booking Name: {bookingDetails.name}</p>
+                <p>Title: {bookingDetails.movieTitle}</p>
+                <p>Showing Details: Screen: {bookingDetails.screen} at {bookingDetails.showingTime} on {bookingDetails.showingDate}</p>
+                <p>Number of Tickets: {bookingDetails.seats.length}</p>
+                <p>Seats: {bookingDetails.seats.join(', ')}</p>
+                <p>Total: £{bookingDetails.total}</p>
+                <Button onClick={() => { setIsLoaded(false) }}>Back</Button>
+            </div>
+        );
+        // // } else {
+        //     return (
+        //         <div>
+        //             <p>No booking found</p>
+        //             <Button onClick={() => { setIsLoaded(false) }}>Back</Button>
+        //         </div>
+        //     )
+        // }
+
+    } else {
+        return (
+            <div>
+                <p>Please enter a booking code or reference:</p>
+                <Input type="text" onChange={(e) => { setCode(e.target.value) }} />
+                <Button onClick={() => { searchType(code) }}>Find My Booking</Button>
+            </div>
+        );
+    }
+}
+
+export default ViewBooking;
