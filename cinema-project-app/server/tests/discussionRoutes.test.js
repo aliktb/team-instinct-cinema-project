3721d041e9959a612545ac829e629d2923548d9b
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-12-14 10:14:38 +0000

    CIN-40 #comment added read and create tests for discussion routes

---
+++ cinema-project-app/server/tests/discussionRoutes.test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const { expect } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const server = require('../server.js');
+chai.use(chaiHttp);
+
+describe('testing discussion routes', function () {
+    it('should be status 201 when creating a new post', function (done) {
+        chai.request(server).post('/posts/create').send({ text: 'this is the text' }).end((err, response) => {
+            expect(response).to.have.status(201);
+            expect(response).to.not.be.null;
+
+            done();
+        });
+    })
+    it('should be status 200 when reading all posts', function (done) {
+        chai.request(server).get('/posts/').end((err, response) => {
+            if (err) {
+                console.log(`error: ${err}`);
+            }
+            expect(response).to.have.status(200);
+            expect(response).to.not.be.null;
+
+            done(err);
+        });
+    })
+    it('should be status 200 when reading posts by id', function (done) {
+        chai.request(server).get('/posts/61afb8fb2805a35cf2d0dd02').end((err, response) => {
+            if (err) {
+                console.log(`error: ${err}`);
+            }
+            expect(response).to.have.status(200);
+            expect(response).to.not.be.null;
+
+            done(err);
+        });
+    })
+    it('should be status 200 when reading posts by movie id', function (done) {
+        chai.request(server).get('/posts/movie/61af96aee0eab7107e5db896').end((err, response) => {
+            if (err) {
+                console.log(`error: ${err}`);
+            }
+            expect(response).to.have.status(200);
+            expect(response).to.not.be.null;
+
+            done(err);
+        });
+    })
+})
