f65446d81e4a3c60c4719ace1ca93463ecad1f61
sean-fernyhough <sean.fernyhough97@gmail.com>
2021-12-15 21:03:05 +0000

    CIN-1 #comment added showing search to all search types

--- cinema-project-app/client/src/Components/Search.jsx
+++ cinema-project-app/client/src/Components/Search.jsx
@@ -8,7 +8,8 @@ const Search = ({ setResults }) => {
   const [searchType, setSearchType] = useState("title");
   const [emptySearch, setEmptySearch] = useState(false);
 
-  const [curLength, setCurLength] = useState(0);
+
+  let curLength;
 
   const arrayCheck = (list) => {
     if (list.length === curLength) {
@@ -25,25 +26,22 @@ const Search = ({ setResults }) => {
         axios
           .get(`http://localhost:3001/movies/title/${searchTerm}`)
           .then((response) => {
-            console.log(response)
             let resultsArray = [];
-            setCurLength(response.data.length);
-            console.log(response.data)
+            // sets how many requests need to complete before results are assigned, based on number of movies from initial request
+            curLength = response.data.length;
             for (let result of response.data) {
-              console.log(result);
               axios.get(`http://localhost:3001/showings/title/e/${result.title}`).then((data) => {
                 if (data.data.length > 0) {
+                  // showings based on if they are before the current day, prevents old showings from appearing
                   let futureShowings = data.data.filter((showing) => { return new Date(showing.date) >= new Date(Date().slice(4, -39)) ? true : false })
 
+                  // Seperates deluxe showings, sorts them based on date and time, finally filters based on the closest date with showings
                   let deluxeShowings = ((futureShowings.filter((showing) => { return showing.screen === "Deluxe" ? true : false })).sort((showing1, showing2) => { return showing1.date > showing2.date })).filter((showing) => { if (String(new Date(showing.date)) === String(new Date(Date().slice(4, -39)))) { return showing.timeRaw > Number(Date().slice(16, -34).replace(':', "")) ? true : false } else { return true } })
                   let filteredDeluxe = deluxeShowings.filter((showing) => { return showing.date > deluxeShowings[0].date ? false : true })
 
-
-
+                  // Seperates standard showings, sorts them based on date and time, finally filters based on the closest date with showings
                   let standardShowings = (futureShowings.filter((showing) => { return showing.screen === "Standard" ? true : false })).sort((showing1, showing2) => { return showing1.date > showing2.date }).filter((showing) => { if (String(new Date(showing.date)) === String(new Date(Date().slice(4, -39)))) { return showing.timeRaw > Number(Date().slice(16, -34).replace(':', "")) ? true : false } else { return true } })
                   let filteredStandard = standardShowings.filter((showing) => { return showing.date > standardShowings[0].date ? false : true })
-
-
 
                   if (deluxeShowings[0]) {
                     result.nextDeluxe = filteredDeluxe;
@@ -51,37 +49,86 @@ const Search = ({ setResults }) => {
                   if (standardShowings[0]) {
                     result.nextStandard = filteredStandard;
                   }
-                  console.log(result)
                   resultsArray.push(result);
                 } else {
                   resultsArray.push(result);
                 }
+                // waits until all requests have finished before assigning results using arrayCheck function
                 arrayCheck(resultsArray);
               })
-
             }
-            // setResults(response.data);
           });
       } else if (searchType === "tags") {
         axios
           .get(`http://localhost:3001/movies/tag/${searchTerm}`)
           .then((response) => {
-            // const movies = response.data;
-            // console.log(response.data);
-            // const filteredtags = movies.filter((movies) => { return movies.tags.includes(searchTerm) })
-            // console.log(filteredtags);
-            // setResults(filteredtags);
-            setResults(response.data);
+            let resultsArray = [];
+            // sets how many requests need to complete before results are assigned, based on number of movies from initial request
+            curLength = response.data.length;
+            for (let result of response.data) {
+              axios.get(`http://localhost:3001/showings/title/e/${result.title}`).then((data) => {
+                if (data.data.length > 0) {
+                  // showings based on if they are before the current day, prevents old showings from appearing
+                  let futureShowings = data.data.filter((showing) => { return new Date(showing.date) >= new Date(Date().slice(4, -39)) ? true : false })
+
+                  // Seperates deluxe showings, sorts them based on date and time, finally filters based on the closest date with showings
+                  let deluxeShowings = ((futureShowings.filter((showing) => { return showing.screen === "Deluxe" ? true : false })).sort((showing1, showing2) => { return showing1.date > showing2.date })).filter((showing) => { if (String(new Date(showing.date)) === String(new Date(Date().slice(4, -39)))) { return showing.timeRaw > Number(Date().slice(16, -34).replace(':', "")) ? true : false } else { return true } })
+                  let filteredDeluxe = deluxeShowings.filter((showing) => { return showing.date > deluxeShowings[0].date ? false : true })
+
+                  // Seperates standard showings, sorts them based on date and time, finally filters based on the closest date with showings
+                  let standardShowings = (futureShowings.filter((showing) => { return showing.screen === "Standard" ? true : false })).sort((showing1, showing2) => { return showing1.date > showing2.date }).filter((showing) => { if (String(new Date(showing.date)) === String(new Date(Date().slice(4, -39)))) { return showing.timeRaw > Number(Date().slice(16, -34).replace(':', "")) ? true : false } else { return true } })
+                  let filteredStandard = standardShowings.filter((showing) => { return showing.date > standardShowings[0].date ? false : true })
+
+                  if (deluxeShowings[0]) {
+                    result.nextDeluxe = filteredDeluxe;
+                  }
+                  if (standardShowings[0]) {
+                    result.nextStandard = filteredStandard;
+                  }
+                  resultsArray.push(result);
+                } else {
+                  resultsArray.push(result);
+                }
+                // waits until all requests have finished before assigning results using arrayCheck function
+                arrayCheck(resultsArray);
+              })
+            }
           });
       } else if (searchType === "cast") {
         axios
           .get(`http://localhost:3001/movies/cast/${searchTerm}`)
           .then((response) => {
-            // const actors = response.data;
-            // console.log(response.data);
-            // const filteredactors = actors.filter((actors) => { return actors.title.includes(searchTerm) })
-            // console.log(filteredactors);
-            setResults(response.data);
+            let resultsArray = [];
+            // sets how many requests need to complete before results are assigned, based on number of movies from initial request
+            curLength = response.data.length;
+            for (let result of response.data) {
+              axios.get(`http://localhost:3001/showings/title/e/${result.title}`).then((data) => {
+                if (data.data.length > 0) {
+                  // showings based on if they are before the current day, prevents old showings from appearing
+                  let futureShowings = data.data.filter((showing) => { return new Date(showing.date) >= new Date(Date().slice(4, -39)) ? true : false })
+
+                  // Seperates deluxe showings, sorts them based on date and time, finally filters based on the closest date with showings
+                  let deluxeShowings = ((futureShowings.filter((showing) => { return showing.screen === "Deluxe" ? true : false })).sort((showing1, showing2) => { return showing1.date > showing2.date })).filter((showing) => { if (String(new Date(showing.date)) === String(new Date(Date().slice(4, -39)))) { return showing.timeRaw > Number(Date().slice(16, -34).replace(':', "")) ? true : false } else { return true } })
+                  let filteredDeluxe = deluxeShowings.filter((showing) => { return showing.date > deluxeShowings[0].date ? false : true })
+
+                  // Seperates standard showings, sorts them based on date and time, finally filters based on the closest date with showings
+                  let standardShowings = (futureShowings.filter((showing) => { return showing.screen === "Standard" ? true : false })).sort((showing1, showing2) => { return showing1.date > showing2.date }).filter((showing) => { if (String(new Date(showing.date)) === String(new Date(Date().slice(4, -39)))) { return showing.timeRaw > Number(Date().slice(16, -34).replace(':', "")) ? true : false } else { return true } })
+                  let filteredStandard = standardShowings.filter((showing) => { return showing.date > standardShowings[0].date ? false : true })
+
+                  if (deluxeShowings[0]) {
+                    result.nextDeluxe = filteredDeluxe;
+                  }
+                  if (standardShowings[0]) {
+                    result.nextStandard = filteredStandard;
+                  }
+                  resultsArray.push(result);
+                } else {
+                  resultsArray.push(result);
+                }
+                // waits until all requests have finished before assigning results using arrayCheck function
+                arrayCheck(resultsArray);
+              })
+            }
           });
       }
     } else {
